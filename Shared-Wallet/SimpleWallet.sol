0ec0000fc636241a71d7fab5fc04b29dfbc4230e
Raunak Singh <raunak1497@gmail.com>
2020-05-11 16:03:57 +0530

    Reduce allowance functionality

--- Shared-Wallet/SimpleWallet.sol
+++ Shared-Wallet/SimpleWallet.sol
@@ -4,7 +4,26 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 contract Simplewallet is Ownable{
     
-    function withdrawMoney(address payable  _to,uint _amount) public onlyOwner {
+    mapping(address=>uint) public allowance;
+    
+    function addAllowance(address _who,uint _amount) public  onlyOwner{
+        allowance[_who] = _amount;
+    }
+    
+    modifier ownerOrAllowed(uint _amount){
+        require((owner() == msg.sender) || (allowance[msg.sender] >= _amount), "You are not allowed");
+        _;
+    }
+    
+    function reduceAllowance(address _who,uint _amount) internal {
+        allowance[_who] -= _amount;
+    }
+    
+    function withdrawMoney(address payable  _to,uint _amount) public ownerOrAllowed(_amount) {
+        require(_amount<= address(this).balance , "Insufficient fund");
+        if( owner() != msg.sender){
+            reduceAllowance(msg.sender,_amount);   
+        }
         _to.transfer(_amount);
     } 
     

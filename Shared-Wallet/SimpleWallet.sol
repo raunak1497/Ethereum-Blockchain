35e4936bc0f50e2c1cf09916ce99e1ef88e7b150
Raunak Singh <raunak1497@gmail.com>
2020-05-11 16:44:04 +0530

    Adding Events

--- Shared-Wallet/SimpleWallet.sol
+++ Shared-Wallet/SimpleWallet.sol
@@ -4,9 +4,14 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 
 contract Allowance is Ownable{
+    
+    event AllowanceChanged(address indexed _forWho,address indexed _fromWhom,uint _oldAmount,uint _newAmount);
+    
      mapping(address=>uint) public allowance;
     
     function addAllowance(address _who,uint _amount) public  onlyOwner{
+        
+        emit AllowanceChanged(_who,msg.sender,allowance[_who],_amount);
         allowance[_who] = _amount;
     }
     
@@ -16,6 +21,7 @@ contract Allowance is Ownable{
     }
     
     function reduceAllowance(address _who,uint _amount) internal {
+        emit AllowanceChanged(_who,msg.sender,allowance[_who], allowance[_who] - _amount);
         allowance[_who] -= _amount;
     }
     
@@ -23,16 +29,19 @@ contract Allowance is Ownable{
 
 contract Simplewallet is Allowance{
     
+   event MoneySent(address indexed benefeciary,uint amount);
+   event MoneyRecieved(address indexed benefeciary,uint amount);
    
     function withdrawMoney(address payable  _to,uint _amount) public ownerOrAllowed(_amount) {
         require(_amount<= address(this).balance , "Insufficient fund");
         if( owner() != msg.sender){
             reduceAllowance(msg.sender,_amount);   
         }
+        emit MoneySent(_to,_amount);
         _to.transfer(_amount);
     } 
     
     receive () external payable {
-        
+        emit MoneyRecieved(msg.sender,msg.value);
     } 
 }

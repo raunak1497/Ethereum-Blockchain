cb8f6afff5dff21674c685bbd91a1a39bf31726a
Raunak Kumar Singh <Raunak Kumar Singh>
2020-08-24 19:59:39 +0530

    Primary Variables

--- Solidity/Variables.sol
+++ Solidity/Variables.sol
@@ -1,46 +1,49 @@
-pragma solidity ^0.6.1;
+pragma solidity *0.6.6;
 
-contract WorkingWithVariables{
+contract MyContract{
     uint256 public myUint;
     
-    function setMyUint(uint _myUint) public {
+    function setMyUint( uint _myUint) public{
         myUint = _myUint;
     }
     
     bool public myBool;
     
-    function setMyBool(bool _myBool) public{
+    function setMyBool(bool _myBool) public {
         myBool = _myBool;
     }
     
     uint8 public myUint8;
     
-    function incrementUint() public{
+    function incrementUint() public {
         myUint8++;
     }
     
-    function decrementUint() public{
+    function decrementUint() public {
         myUint8--;
     }
     
     address public myAddress;
     
     function setAddress(address _myAddress) public{
-        myAddress =_myAddress;
+        myAddress = _myAddress;
     }
     
-    function getBalanceOfAddress() public view returns(uint) {
+    // view function are used to view and it returns
+    function getBalanceAddress() public view returns(uint){
+        //every address has a balance associated to it
         return myAddress.balance;
     }
     
-    string public myString = 'surprise mf';
-    
-    function setMyString(string memory _myString) public{
-        myString = _myString;
-    }
-
-    // function getBalance() public view returns(uint){
-    //     return address(this).balance;
-    // } 
+    //strings are byte array in solidity
+     
+     string public myString = 'Hello world';
+      
+      //we need to specify where is the string stored so we specify memory
+      function setString(string memory _myString) public{
+          myString = _myString;
+      }
+      
+      
+     
 }
-
